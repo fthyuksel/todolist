13995471bfd58e9a994e515d7c300bbf1319c695
Fatih <fthyuksel61@gmai.com>
2021-04-29 18:41:29 +0300

    To Do List anasayfa kaba tasarımı

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,12 +1,32 @@
 <template>
-  <card :title="$t('home')">
-    {{ $t('you_are_logged_in') }}
+  <card>
+
+    <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+      <h1 class="text-black-50 display-6">To Do List</h1>
+    </div>
+    <form>
+      <div class="input-group mb-3">
+        <input type="text" class="form-control form-control-lg" placeholder="Add something your todo List" aria-label="arialabel" aria-describedby="button-addon2">
+        <div class="input-group-append">
+          <button class="btn btn-success" type="submit" id="button-addon2">Add ToDo</button>
+        </div>
+      </div>
+    </form>
+
   </card>
 </template>
 
 <script>
 export default {
   middleware: 'auth',
+
+  data(){
+    return{
+
+    }
+  },
+  mounted() {
+  },
 
   metaInfo () {
     return { title: this.$t('home') }
