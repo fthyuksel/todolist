b5e523ab4fe8d1ceb179c77185672713b4fd8b7d
Fatih <fthyuksel61@gmai.com>
2021-04-29 19:36:51 +0300

    Gerekli model migration ve controller yapısının kurulması, ilk veritabanı todo yollanması

--- resources/js/pages/home.vue
+++ resources/js/pages/home.vue
@@ -1,31 +1,47 @@
 <template>
   <card>
-
     <div class="w-100 h-100 d-flex flex-column justify-content-center align-items-center">
-      <h1 class="text-black-50 display-6">To Do List</h1>
+      <h1 class="text-black-50 display-6">
+        To Do List
+      </h1>
     </div>
-    <form>
+    <form @submit.prevent="saveData">
       <div class="input-group mb-3">
-        <input type="text" class="form-control form-control-lg" placeholder="Add something your todo List" aria-label="arialabel" aria-describedby="button-addon2">
+        <input v-model="form.title" type="text" class="form-control form-control-lg" placeholder="Add something your todo List" aria-label="arialabel" aria-describedby="button-addon2">
         <div class="input-group-append">
-          <button class="btn btn-success" type="submit" id="button-addon2">Add ToDo</button>
+          <button id="button-addon2" class="btn btn-success" type="submit">
+            Add ToDo
+          </button>
         </div>
       </div>
     </form>
-
   </card>
 </template>
 
 <script>
+import Form from '../Form'
+
+window.axios = require('axios');
+
+
 export default {
   middleware: 'auth',
 
-  data(){
-    return{
-
+  data () {
+    return {
+      form: new Form({
+        title: ''
+      })
     }
   },
-  mounted() {
+  mounted () {
+  },
+  methods: {
+    saveData () {
+      const data = new FormData()
+      data.append('title', this.form.title)
+      axios.post('/api/todo',data)
+    }
   },
 
   metaInfo () {
